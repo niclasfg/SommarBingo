daf9a88e8b6fcbf360078b97a5d8caba66445302
Niclas Geuken <niclas.fg@gmail.com>
2022-10-01 20:45:42 +0200

    4x4 grid for bingo, add text via js

--- bingo.js
+++ bingo.js
@@ -1,1 +1,27 @@
 console.log("Bingo");
+
+
+
+function bingoText(bingoBox) {   
+    var bingoText = `
+    '<div class='bingoText'>
+    <h1> Uppgift </h1>
+    <p>Beskrivning av bingorutan som ska utföras.</p>
+    </div>
+    `;
+    bingoBox.innerHTML = bingoText;
+}
+
+
+function NameBingo() {
+    var name = prompt("Vad heter du?")
+    const header = document.querySelector(".header");
+    header.textContent = name.toUpperCase() + "\nSOMMARBINGO\n2023";
+}
+    
+
+
+const bingoBoxes = document.querySelectorAll(".bingoBox");
+bingoBoxes.forEach((box) => {
+    bingoText(box);
+});
